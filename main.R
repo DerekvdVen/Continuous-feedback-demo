da958879c103046bd7e04c4fcf9598a66c74f7ab
Oosterhuis, Thomas <thomas.oosterhuis@wur.nl>
2019-05-20 11:44:52 +0200

    Exercise template added

---
+++ main.R
@@ -0,0 +1,30 @@
+# Geoscripting 2020 
+# Lesson 1, Exercise 1
+# Leap year
+# TEAMNAME
+# NAME OF TEAM MEMBERS
+# DATE
+
+# Import packages
+library(MyPackage)
+
+# Source functions
+source('R/MyFunction.R')
+
+# Demonstrate function
+MyFunction(example1)
+
+MyFunction(example2)
+
+MyFunction(example3)
+
+# Demonstrate error handling
+MyFunction(example4)
+
+MyFunction(example5)
+
+MyFunction(example6)
+
+
+
+
